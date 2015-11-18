016520a71b9b6951a04e8acae8cda31ed5d2170c
Moritz Schilling <moritz.schilling@dfki.de>
2015-11-18 17:26:27 +0100

    Fixed bug in WSUM -> VERIFIED
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- vhdl/bg_merge_wsum.vhd
+++ vhdl/bg_merge_wsum.vhd
@@ -59,6 +59,9 @@ architecture Behavioral of bg_merge_wsum is
     signal fp_div_result : std_logic_vector(DATA_WIDTH-1 downto 0);
     signal fp_div_start : std_logic;
     signal fp_div_rdy : std_logic;
+    signal fp_result : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal fp_add3_start : std_logic;
+    signal fp_add3_rdy : std_logic;
     signal fp_accumulate : std_logic_vector(1 downto 0);
 
     signal fp_in_req : std_logic;
@@ -104,6 +107,16 @@ begin
                     start_i => fp_div_start,
                     ready_o => fp_div_rdy
                  );
+        fp_add3 : entity work.fpu_add(rtl)
+        port map (
+                    clk_i => clk,
+                    opa_i => fp_div_result,
+                    opb_i => in_bias,
+                    rmode_i => "00", -- round to nearest even
+                    output_o => fp_result,
+                    start_i => fp_add3_start,
+                    ready_o => fp_add3_rdy
+                 );
 
         InputProcess : process(clk)
         begin
@@ -125,7 +138,7 @@ begin
                             if (in_req(currInput) = '1' and halt = '0') then
                                 -- pass incoming data to opa
                                 fp_opa1 <= in_port(currInput);
-                                fp_opa2 <= in_weights(currInput);
+                                fp_opa2 <= "0" & in_weights(currInput)(DATA_WIDTH-2 downto 0); -- abs value
                                 in_ack(currInput) <= '1';
                                 InputState <= waiting;
                             end if;
@@ -172,6 +185,7 @@ begin
                     fp_in_ack <= '0';
                     fp_add1_start <= '0';
                     fp_add2_start <= '0';
+                    fp_add3_start <= '0';
                     fp_div_start <= '0';
                     accumulate := "00";
                     fp_acc1 <= (others => '0');
@@ -182,6 +196,7 @@ begin
                     fp_in_ack <= '0';
                     fp_add1_start <= '0';
                     fp_add2_start <= '0';
+                    fp_add3_start <= '0';
                     fp_div_start <= '0';
                     CalcState <= CalcState;
                     case CalcState is
@@ -210,13 +225,11 @@ begin
                             end if;
                         when computing1 =>
                             if (fp_div_rdy = '1') then
-                                fp_opa1 <= in_bias;
-                                fp_acc1 <= fp_div_result;
-                                fp_add1_start <= '1';
+                                fp_add3_start <= '1';
                                 CalcState <= computing2;
                             end if;
                         when computing2 =>
-                            if (fp_add1_rdy = '1') then
+                            if (fp_add3_rdy = '1') then
                                 fp_out_req <= '1';
                                 CalcState <= pushing;
                             end if;
@@ -246,7 +259,7 @@ begin
                     case OutputState is
                         when idle =>
                             if (fp_out_req = '1') then
-                                out_port <= fp_add1_result;
+                                out_port <= fp_result;
                                 fp_out_ack <= '1';
                                 OutputState <= pushing;
                             end if;
